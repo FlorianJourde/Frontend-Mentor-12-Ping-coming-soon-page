d0878f51523dc87336629f80909759fff51af22e
Florian Jourde <jourdeflorian@gmail.com>
2021-10-27 12:38:59 +0200

    Challenge submission

---
+++ script.js
@@ -0,0 +1,25 @@
+const email = document.getElementById("email")
+const form = document.querySelector("form");
+
+function validateEmail(email) {
+  const regex =
+    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(email);
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (!validateEmail(email.value)) {
+    email.classList.add("alert");
+  } else {
+    if (email.classList.contains('alert')) {
+      email.classList.remove("alert");
+      email.addEventListener('webkitTransitionEnd', function() {
+        email.classList.add("sent");
+      });
+    } else {
+      email.classList.add("sent");
+    }
+  }
+});
